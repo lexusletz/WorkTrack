91980f9fd0eb641089c5d0396867607f47f0d967
Jordy Pinos <jordypinos3@gmail.com>
2026-06-16 22:35:55 +0200

    chore: Update imports and names

--- lib/core/preferences/preferences_providers.dart
+++ lib/core/preferences/preferences_providers.dart
@@ -1,29 +1,48 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'data_sources/local_preferences_data_source.dart';
 import 'preferences_model.dart';
-import 'preferences_repository.dart';
+import 'preferences_orchestrator.dart';
+import 'data_sources/remote_preferences_data_source.dart';
 
 final sharedPrefsProvider = Provider<SharedPreferences>((_) {
   throw UnimplementedError();
 });
 
-final preferencesRepositoryProvider = Provider<PreferencesRepository>((ref) {
-  final prefs = ref.watch(sharedPrefsProvider);
+final localPreferencesRepositoryProvider = Provider<LocalPreferencesDataSource>(
+  (ref) {
+    final prefs = ref.watch(sharedPrefsProvider);
 
-  return PreferencesRepository(prefs);
+    return LocalPreferencesDataSource(prefs);
+  },
+);
+
+final remotePreferencesRepositoryProvider =
+    Provider<RemotePreferencesDataSource>((ref) {
+      return RemotePreferencesDataSource();
+    });
+
+final preferencesOrchestratorProvider = Provider<PreferencesOrchestrator>((
+  ref,
+) {
+  final local = ref.watch(localPreferencesRepositoryProvider);
+  final remote = ref.watch(remotePreferencesRepositoryProvider);
+
+  return PreferencesOrchestrator(local, remote);
 });
 
 class PreferencesNotifier extends AsyncNotifier<Preferences> {
   @override
   Future<Preferences> build() =>
-      ref.read(preferencesRepositoryProvider).loadPreferences();
+      ref.read(preferencesOrchestratorProvider).load();
 
   Future<void> savePreferences(Preferences next) async {
     state = AsyncData(next);
-    await ref.read(preferencesRepositoryProvider).save(next);
+    await ref.read(preferencesOrchestratorProvider).save(next);
   }
 }
 
-final preferencesProvider = AsyncNotifierProvider<PreferencesNotifier, Preferences>(
-  PreferencesNotifier.new,
-);
+final preferencesProvider =
+    AsyncNotifierProvider<PreferencesNotifier, Preferences>(
+      PreferencesNotifier.new,
+    );
